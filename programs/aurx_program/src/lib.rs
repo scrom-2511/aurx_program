84dd66962f552f007e446043a707791878c4cabb
scrom <formain337@gmail.com>
2025-12-29 14:11:07 +0000

    add: store_hash fn with all the structs needed

--- programs/aurx_program/src/lib.rs
+++ programs/aurx_program/src/lib.rs
@@ -6,11 +6,23 @@ declare_id!("5piAYhnjtWG6bnpkX3q18rNzKNgt3536xq47w3VS5uuV");
 pub mod aurx_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn store_hash(ctx: Context<StoreHash>, user_email: String, file_id: String, hash: String) -> Result<()> {
+        ctx.accounts.storage_pda.hash = hash;
         Ok(())
     }
 }
 
+#[account]
+pub struct HashStorage {
+    hash: String,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(user_email:String, file_id: String)]
+pub struct StoreHash<'info> {
+    #[account(init, payer = signer, space = 8+4+50, seeds=[signer.key().as_ref(), user_email.as_bytes(), file_id.as_bytes()], bump)]
+    pub storage_pda: Account<'info, HashStorage>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
